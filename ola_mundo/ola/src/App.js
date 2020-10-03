af2dc2e937808ccb7245fe5ccceca6f52a0461cf
Natan <natanlucas099@hotmail.com>
2020-10-03 10:31:54 -0300

    Meu priemeiro ola mundo

---
+++ ola_mundo/ola/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Olá Mundo 
+      Esse é o meu primeiro Site!!
+    </div>
+  );
+}
+
+export default App;
